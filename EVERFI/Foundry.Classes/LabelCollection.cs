982e4b4c546fae88312331e51e04925ec0992857
abbepiels <apiels19@gmail.com>
2021-06-16 12:57:16 -0400

    added label collection

---
+++ EVERFI/Foundry.Classes/LabelCollection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVERFI.Foundry.Classes
+{
+    internal enum names
+    {
+        
+    }
+    internal enum ids
+    {
+
+    }
+    internal enum categoryNames
+    {
+
+    }
+    public class LabelCollection
+    {
+        internal names name { get; set; }
+        internal ids id { get; set; }
+        internal categoryNames category { get; set; }
+
+
+    }
+
+}
+   
