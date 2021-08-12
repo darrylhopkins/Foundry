9e0bad7720cfbfc56b621908db83575aee3fbc35
abbepiels <40579741+abbepiels@users.noreply.github.com>
2021-08-11 23:59:12 -0400

    new files for program user and user progress

---
+++ EVERFI/Foundry/API.ProgramUsers.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using EVERFI.Foundry.Classes;
+using RestSharp.Authenticators;
+using System.Linq;
+using System.Globalization;
+
+namespace EVERFI.Foundry
+{
+    public partial class API
+    {
+        public (List<ProgramUser>, bool) GetProgramUsers(int scrollSize)
+        {
+            bool keepGoing = true;
+            List<ProgramUser> programUserList = new List<ProgramUser>();
+            RestRequest request = new RestRequest();
+            request.Parameters.Clear();
+            request.Method = Method.GET;
+            request.AddParameter("version", _ver, ParameterType.UrlSegment);
+            request.Resource = "{version}/progress/program_users";
+            request.AddParameter("since", since, ParameterType.QueryString);
+            request.AddParameter("scroll_size", scrollSize, ParameterType.QueryString);
+            request.AddParameter("scroll_id", scrollId, ParameterType.QueryString);
+            request.AddHeader("Content-Type", "application/json");
+
+            IRestResponse response = _client.Execute(request);
+
+
+            checkResponseSuccess(response);
+            NextUserData next = JsonConvert.DeserializeObject<NextUserData>(response.Content);
+            ProgramUserDataHeaderList programUserData = JsonConvert.DeserializeObject<ProgramUserDataHeaderList>(response.Content);
+            if (next.Next.ScrollId != null)
+            {
+                DateTime sinceDate = next.Next.Since;
+                since = sinceDate.ToString("yyyy-MM-dd'T'HH:mm:ss.fffK", CultureInfo.InvariantCulture);
+                scrollId = next.Next.ScrollId;
+            }
+            else
+            {
+
+                keepGoing = false;
+
+            }
+            foreach (ProgramUser program in programUserData.ProgressDataHeaderList)
+            {
+                programUserList.Add(program);
+            }
+
+
+            return (programUserList, keepGoing);
+        }
+
+    }
+}
---
+++ EVERFI/Foundry/API.UserProgress.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using EVERFI.Foundry.Classes;
+using RestSharp.Authenticators;
+using System.Linq;
+using System.Globalization;
+
+namespace EVERFI.Foundry
+{
+	public partial class API
+	{
+        public (List<UserProgress>, bool) GetUserProgress(int scrollSize)
+        {
+
+            bool keepGoing = true;
+            List<UserProgress> userProgressList = new List<UserProgress>();
+            RestRequest request = new RestRequest();
+            request.Method = Method.GET;
+            request.Parameters.Clear();
+            request.AddParameter("version", _ver, ParameterType.UrlSegment);
+            request.Resource = "{version}/progress/user_assignments";
+            request.AddParameter("since", since, ParameterType.QueryString);
+            request.AddParameter("scroll_id", scrollId, ParameterType.QueryString);
+            request.AddParameter("scroll_size", scrollSize, ParameterType.QueryString);
+            request.AddHeader("Content-Type", "application/json");
+
+            IRestResponse response = _client.Execute(request);
+            checkResponseSuccess(response);
+
+            NextUserData next = JsonConvert.DeserializeObject<NextUserData>(response.Content);
+
+            if (next.Next.ScrollId != null)
+            {
+                DateTime sinceDate = next.Next.Since;
+                since = sinceDate.ToString("yyyy-MM-dd'T'HH:mm:ss.fffK", CultureInfo.InvariantCulture);
+                scrollId = next.Next.ScrollId;
+            }
+            else
+            {
+                keepGoing = false;
+            }
+
+            UserProgressDataHeaderList progressData = JsonConvert.DeserializeObject<UserProgressDataHeaderList>(response.Content);
+            foreach (UserProgress data in progressData.ProgressDataHeaderList)
+            {
+                userProgressList.Add(data);
+
+            }
+            return (userProgressList, keepGoing);
+
+        }
+	}
+}
